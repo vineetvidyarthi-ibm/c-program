43530e7c8b8ddd2794e097ab579df6fc4886efac
vineet vidyarthi <vineet.vidyarthi@rungta.org>
2025-12-29 22:36:40 +0530

    Add files via upload

---
+++ lab experiments/lab1.c
@@ -0,0 +1,8 @@
+//first C program print name/age using printf.
+#include <stdio.h>
+int main() {
+    // Print name and age
+    printf("Name: Vineet Vidyarthi\n");
+    printf("Age: 25\n");
+    return 0;
+}
